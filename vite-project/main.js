be08d17ad4e97c4d0a3623584489e95027b9deea
LydiaZ07 <zhulydia07@gmail.com>
2022-12-20 13:10:15 -0500

    l

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,7 @@
 const DOMSelectors = {
-    
-}
+    buttons: document.getElementById("buttons"),
+    btn: document.querySelectorAll("btn"),
+    objects: document.getElementById("objects"),
+    object: document.querySelectorAll("object")
+};
+
